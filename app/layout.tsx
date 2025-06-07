9a625d0d39ac7b7be5e63c25ae90f5e0364d95bf
Mamun-Miah <mahadyhasen7@gmail.com>
2025-06-07 22:33:21 +0600

    AOS Removed fix 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from './components/Header';
 import Footer from "./components/Footer";
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
