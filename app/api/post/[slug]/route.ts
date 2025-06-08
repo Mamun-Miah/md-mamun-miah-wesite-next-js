2a89430d80f1791243ff8c0972b5c4b02fa89e37
Mamun-Miah <mahadyhasen7@gmail.com>
2025-06-09 00:04:55 +0600

    Blog setup19

--- app/api/post/[slug]/route.ts
+++ app/api/post/[slug]/route.ts
@@ -1,29 +1,6 @@
-import { NextRequest } from 'next/server';
-
-export const runtime = 'edge';
-
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { slug: string } }
-) {
-  const { slug } = params;
-
-  const wpRes = await fetch(
-    `https://lightblue-goat-212889.hostingersite.com/wp-json/wp/v2/posts?slug=${slug}&_embed`
-  );
-
-  if (!wpRes.ok) {
-    return new Response(`Failed to fetch WordPress post: ${wpRes.status}`, {
-      status: wpRes.status,
-    });
-  }
-
-  const data = await wpRes.json();
-
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+export async function GET() {
+  const data = await fetch('https://lightblue-goat-212889.hostingersite.com/wp-json/wp/v2/posts?slug=${slug}&_embed')
+  const posts = await data.json()
+ 
+  return Response.json(posts)
 }
