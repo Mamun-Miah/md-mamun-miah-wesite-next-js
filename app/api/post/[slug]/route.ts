ed849e27e028191e0d9bbb69b648e642552b4629
Mamun-Miah <mahadyhasen7@gmail.com>
2025-06-09 00:07:48 +0600

    Blog setup20

--- app/api/post/[slug]/route.ts
+++ app/api/post/[slug]/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 export async function GET() {
   const data = await fetch('https://lightblue-goat-212889.hostingersite.com/wp-json/wp/v2/posts?slug=${slug}&_embed')
   const posts = await data.json()
