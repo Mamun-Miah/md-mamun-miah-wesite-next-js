29efa0b94d947770a100737a8bbf1d937479388a
Mamun-Miah <mahadyhasen7@gmail.com>
2025-06-08 23:43:30 +0600

    Blog setup16

--- app/api/post/[slug]/route.ts
+++ app/api/post/[slug]/route.ts
@@ -1,16 +1,18 @@
-export async function GET(request: Request, context: { params: { slug: string } }) {
-  const { slug } = context.params;
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+  const { slug } = params;
 
   const wpRes = await fetch(
     `https://lightblue-goat-212889.hostingersite.com/wp-json/wp/v2/posts?slug=${slug}&_embed`
   );
 
   if (!wpRes.ok) {
-    return new Response(`Failed to fetch WordPress post: ${wpRes.status}`, {
+    return new NextResponse(`Failed to fetch WordPress post: ${wpRes.status}`, {
       status: wpRes.status,
     });
   }
 
   const data = await wpRes.json();
-  return Response.json(data);
+  return NextResponse.json(data);
 }
--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-70 z-50">
-      <span className="loading loading-ring loading-xl text-primary"></span>
+      <span className="loading loading-ring w-24 text-warning"></span>
     </div>
   );
 };
