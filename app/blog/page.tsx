47869a6b90e9fd2e39b322794c30f6c32197cee1
Mamun-Miah <mahadyhasen7@gmail.com>
2025-06-10 22:19:18 +0600

    Blog setup69

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default async function BlogPage() {
   const res = await fetch(
-    'https://raw.githubusercontent.com/Mamun-Miah/WordPress-API-Automation-to-Github/refs/heads/main/posts.json'
+    'https://linen-squirrel-954851.hostingersite.com/posts.json'
   );
 
   if (!res.ok) {
