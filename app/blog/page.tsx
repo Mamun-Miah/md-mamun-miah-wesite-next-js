1c2a1598d55cb1c0c26aac7ea0d281bea0150978
Mamun-Miah <mahadyhasen7@gmail.com>
2025-06-11 08:39:32 +0600

    Blog setup78

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -31,7 +31,7 @@ export default async function BlogPage({ searchParams }: Props) {
   const currentPage = parseInt(params?.page || '1', 10);
 
   // Wait for 1 second
-  await delay(1000);
+  await delay(10);
 
   const res = await fetch(
     'https://raw.githubusercontent.com/Mamun-Miah/WordPress-API-Automation-to-Github/refs/heads/main/posts.json'
