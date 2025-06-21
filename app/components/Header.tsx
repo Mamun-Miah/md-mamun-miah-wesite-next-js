8696cb58a9c0a17c707663a3105a11ef37a45ed2
Mamun-Miah <mahadyhasen7@gmail.com>
2025-06-21 23:55:23 +0600

    updated loading state2

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC = () => {
       className="navbar bg-white/20 backdrop-blur-md rounded-lg mt-5 px-5">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div tabIndex={0} role="button" aria-label="menu mobile" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5 text-white"
