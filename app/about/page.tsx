fa33774b74778937eae58484d947a37e357b58fc
Mamun-Miah <mahadyhasen7@gmail.com>
2026-01-28 23:05:57 +0600

    updated about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,12 @@ const About = () => {
     
     <Hero
         title="About Me"
-        description="My journey in SEO and web development began over 7 years ago when I discovered the power of search engines in driving business success. From working with small startups to large enterprises, I've learned how to craft SEO strategies that deliver real results. My goal is to help businesses achieve online growth through technical expertise and data-driven decisions."
+        description="
+            I am a Full Stack Web Developer with over 7 years of experience building scalable, high-performance websites and web applications.
+
+            I specialize in both front-end and back-end development, creating secure, responsive, and user-friendly digital solutions. Beyond development, I integrate advanced SEO strategies directly into the development process ensuring websites are optimized for speed, visibility, and long-term growth from day one.
+
+            I am passionate about modern web technologies, clean code, and continuous learning. I enjoy solving complex problems, improving performance, and aligning technical solutions with real business objectives."
         backgroundImage="images/about-us-bg.png"
       />
   )
